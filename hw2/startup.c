55127e74c7f752ed88909b77a89bf777945d66ab
Austin Guiney <aguiney@cs.umb.edu>
2021-10-22 16:07:19 -0400

    Changed main() call to init_kernel().

--- hw2/startup.c
+++ hw2/startup.c
@@ -2,10 +2,11 @@
 
 extern void clr_bss(void);
 extern void init_devio(void);
-extern void main(void);
+extern void init_kernel(void);
+
 void _startupc()
 {
-  clr_bss();			/* clear BSS area (uninitialized data) */
-  init_devio();			/* latch onto Tutor-supplied info, code */
-  (void)main();			/* execute user-supplied main */
+  clr_bss();			        /* clear BSS area (uninitialized data) */
+  init_devio();			      /* latch onto Tutor-supplied info, code */
+  init_kernel();			    /* execute user-supplied main */
 }
