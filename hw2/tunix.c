af0686cd80cb5f8a1fec5ff82326c4987dbe01c5
Austin Guiney <aguiney@cs.umb.edu>
2021-10-25 14:56:20 -0400

    Implemented the trap service routine for write.

--- hw2/tunix.c
+++ hw2/tunix.c
@@ -1,8 +1,25 @@
+#include <cpu.h>
+#include <gates.h>
+#include "tsystm.h"
+
+extern IntHandler syscall;
 extern void ioinit(void);
 extern void main(void);
 
 void init_kernel()
 {
   ioinit();
+  set_trap_gate(0x80,&syscall);
   (void)main();
+}
+
+void syscallc(int user_eax, int devcode, char *buf, int bufflen)
+{
+  int trap_code = user_eax;
+
+  switch (trap_code) {
+    case 4:
+      syswrite(devcode, buf, bufflen);
+      break;
+  }
 }
