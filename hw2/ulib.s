9522d43e5218ef7bc05fd934e278a01f3994a4c7
Austin Guiney <aguiney@cs.umb.edu>
2021-10-25 14:55:51 -0400

    Changed some formatting.

--- hw2/ulib.s
+++ hw2/ulib.s
@@ -15,15 +15,16 @@
 # esp->  saved-ebx
 # 4(esp) return addr
 # 8(esp) first arg  (to be put in ebx for syscall)
-#12(esp) second arg (to be put in ecx)
-#16(esp) third arg  (to be put in edx)
+# 12(esp) second arg (to be put in ecx)
+# 16(esp) third arg  (to be put in edx)
 			
-write:	pushl %ebx                    # save the value of ebx
+write:
+	pushl %ebx                	  # save the value of ebx
 	movl 8(%esp),%ebx             # first arg in ebx
 	movl 12(%esp),%ecx            # second arg in ecx
 	movl 16(%esp),%edx            # third arg in edx
 	movl $4,%eax                  # syscall # in eax
-        int $0x80                     # trap to kernel
+	int $0x80                     # trap to kernel
 	popl  %ebx                    # restore the value of ebx
 	ret
 
