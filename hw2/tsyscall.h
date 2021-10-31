7fa0ca414ae94ae1577a275bceb516ec7e69e1e9
Austin Guiney <aguiney@cs.umb.edu>
2021-10-31 15:25:28 -0400

    Created a header file for the trap code numbers.

---
+++ hw2/tsyscall.h
@@ -0,0 +1,9 @@
+#ifndef TSYSCALL_H
+#define TSYSCALL_H
+
+/* Trap code numbers for exit, read, and write. */
+#define EXIT_TRAP_NUMBER 1
+#define READ_TRAP_NUMBER 3
+#define WRITE_TRAP_NUMBER 4
+
+#endif
