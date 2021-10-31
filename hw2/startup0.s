d086575987772a8492ba0081c6839f33e9c91ee3
Austin Guiney <aguiney@cs.umb.edu>
2021-10-31 15:24:00 -0400

    Modified spacing of the comments.

--- hw2/startup0.s
+++ hw2/startup0.s
@@ -1,10 +1,11 @@
 # asm startup file
 # very first module in load
 
-.globl _start, _startupc
-_start:   movl $0x3ffff0, %esp   # set user program stack
-          movl $0, %ebp          # make debugger backtrace terminate here
-          call _startupc         # call C startup, which calls user main
-_exit:    int $3                 # return to Tutor
+.globl _start, _startupc, shutdown
+
+_start:     movl $0x3ffff0, %esp   # set user program stack
+            movl $0, %ebp          # make debugger backtrace terminate here
+            call _startupc         # call C startup, which calls user main
+shutdown:   int $3                 # return to Tutor
 
 
