756a42e0caa5568d3d831a1ca399a4f87087a77d
Austin Guiney <aguiney@cs.umb.edu>
2021-10-31 15:26:05 -0400

    Replaced the test program with the testio.c program from HW1.

--- hw2/test1.c
+++ hw2/test1.c
@@ -1,16 +1,79 @@
 #include <stdio.h>
 #include "tty_public.h"
 #include "tsystm.h"
-#include "tunistd.h"
-#define BUFLEN 20
+
+#define MILLION 1000000
+#define DELAYLOOPCOUNT (400 * MILLION)
+#define BUFLEN 80
+
+void delay(void);
 
 int main(void)
 {
   char buf[BUFLEN];
-  int got;
+  int got, i, lib_console_dev, ldev;
 
-  kprintf("Trying to get a minimum viable setup.\n");
-  kprintf("Trying simple write(4 chars)...\n");
-  got = syswrite(TTY1,"hi!\n",4);
+  /* Determine the SAPC's "console" device, the serial port for user i/o */
+  lib_console_dev = sys_get_console_dev();  /* SAPC support lib fn */
+  switch(lib_console_dev) {
+  case COM1: ldev = TTY0;	/* convert to our dev #'s */
+    break;
+  case COM2: ldev = TTY1;
+    break;
+  default: printf("Unknown console device\n");
+    return 1;
+  }
+  kprintf("Running with device TTY%d\n",ldev);
+  /* Now have a usable device to talk to with i/o package-- */
+  syscontrol(ldev, ECHOCONTROL, 1);
+  kprintf("\nTrying simple write(4 chars)...\n");
+  got = syswrite(ldev,"hi!\n",4);
   kprintf("write of 4 returned %d\n",got);
+  delay();  /* time for output to finish, once write-behind is working */
+
+  kprintf("Trying longer write (9 chars)\n");
+  got = syswrite(ldev, "abcdefghi", 9);
+  kprintf("write of 9 returned %d\n",got);
+  delay();  /* time for output to finish, once write-behind is working */
+
+  for (i = 0; i < BUFLEN; i++)
+    buf[i] = 'A'+ i/2;
+  kprintf("\nTrying write of %d-char string...\n", BUFLEN);
+  got = syswrite(ldev, buf, BUFLEN);
+  kprintf("\nwrite returned %d\n", got);
+  delay();
+
+  kprintf("\nType 10 chars input to test typeahead while looping for delay...\n");
+  delay();
+  got = sysread(ldev, buf, 10);	/* should wait for all 10 chars, once fixed */
+  kprintf("\nGot %d chars into buf. Trying write of buf...\n", got);
+  syswrite(ldev, buf, got);
+
+  kprintf("\nTrying another 10 chars read right away...\n");
+  got = sysread(ldev, buf, 10);	/* should wait for input, once fixed */
+  kprintf("\nGot %d chars on second read\n",got);
+  if (got == 0)
+    kprintf("nothing in buffer\n");	/* expected result until fixed */
+  else 
+    syswrite(ldev, buf, got);	/* should write 10 chars once fixed */
+
+  kprintf("\n\nNow turning echo off--\n");
+  syscontrol(ldev, ECHOCONTROL, 0);
+  kprintf("\nType 20 chars input, note lack of echoes...\n");
+  delay();
+  got = sysread(ldev, buf, 20);
+  kprintf("\nTrying write of buf...\n");
+  syswrite(ldev, buf, got);
+  kprintf("\nAsked for 20 characters; got %d\n", got);
+
+  return 0;
+}
+
+void delay(void)
+{
+  int i;
+
+  kprintf("<doing delay>\n");
+  for (i = 0; i < DELAYLOOPCOUNT; i++)
+   ;
 }
