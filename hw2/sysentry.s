8b2c7dfc2ab379ff2221430b5215fb100ba041d2
Austin Guiney <aguiney@cs.umb.edu>
2021-10-31 15:24:22 -0400

    Added comments.

--- hw2/sysentry.s
+++ hw2/sysentry.s
@@ -1,13 +1,13 @@
 .globl syscallc, syscall
 
 syscall:
-    pushl %edx
+    pushl %edx    # push the values of eax to edx to stack
     pushl %ecx
     pushl %ebx
     pushl %eax
-    call syscallc
-    popl %eax
+    call syscallc # call c trap routine in tunix.c
+    popl %eax     # pop the values of eax to edx from stack
     popl %ebx
     popl %ecx
     popl %edx
-    iret
+    iret          # return to caller
