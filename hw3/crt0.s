df7eb37f318c1e59a83023994c9e63cf2a778ecf
Austin Guiney <aguiney@cs.umb.edu>
2021-11-16 21:51:41 -0500

    Modified spacing.

--- hw3/crt0.s
+++ hw3/crt0.s
@@ -6,10 +6,10 @@
 .globl ustart, main , exit
 .text
 			
-ustart:	        movl $0x3ffff0, %esp         #reinit the stack
+ustart:	        movl $0x3ffff0, %esp         # reinit the stack
 		movl $0, %ebp                # and frame pointer
-		call main                   #call main in the uprog.c
-                pushl %eax                   #push the retval=exit_code on stack
-                call exit                   # call sysycall exit
+		call main                   # call main in the uprog.c
+                pushl %eax                   # push the retval=exit_code on stack
+                call exit                    # call sysycall exit
                
 
