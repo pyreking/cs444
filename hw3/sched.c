81d49b9004a5b8ba5d8fc67997ca19a2bbf02109
Austin Guiney <aguiney@cs.umb.edu>
2021-11-20 17:20:23 -0500

    Added process switching code.

--- hw3/sched.c
+++ hw3/sched.c
@@ -1,21 +1,48 @@
 #include "sched.h"
 #include "proc.h"
-extern void ustart1(void);
-extern void ustart2(void);
-extern void ustart3(void);
+#include <cpu.h>
+#include <stdio.h>
+
+#define BUFLEN 200
+
+extern void asmswtch(PEntry *oldproc, PEntry *curproc);
+extern void debug_log(char * msg);
+void log_process_switch(PEntry *oldproc);
 
 void schedule(void) {
+  PEntry *oldproc = curproc;
+
+  cli();
+
   for (int i = 1; i < NPROC; i++) {
     if (proctab[i].p_status == RUN) {
-      if (i == 1) {
-        ustart1();
-      }
-      if (i == 2) {
-        ustart2();
-      }
-      if (i == 3) {
-        ustart3();
-      }
+      curproc = &proctab[i];
+      log_process_switch(oldproc);
+      asmswtch(oldproc, curproc);
     }
+  }
+  
+  sti();
+}
+
+void log_process_switch(PEntry *oldproc) {
+  char buf[BUFLEN];
+
+  switch (oldproc->p_status) {
+    case ZOMBIE:
+      sprintf(buf, "|(%dz-%d)", oldproc - proctab, curproc - proctab);
+      break;
+
+    case BLOCKED:
+      sprintf(buf, "|(%db-%d)", oldproc - proctab, curproc - proctab);
+      break;
+
+    default:
+      sprintf(buf, "|(%d-%d)", oldproc - proctab, curproc - proctab);
+      break;
+  }
+
+  if (strlen(buf) > 0) {
+      debug_log(buf);
   }
 }
