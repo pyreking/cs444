1c2dcf391eb2670e39918c74f8f7c67cb32c62d8
Austin Guiney <aguiney@cs.umb.edu>
2021-11-22 19:31:07 -0500

    Added function for process0 and fixed a bug where a page fault would occur if the scheduler tried to switch to process 0.

--- hw3/tunix.c
+++ hw3/tunix.c
@@ -1,21 +1,30 @@
 /* file: tunix.c core kernel code */
 
-#include <stdio.h>
 #include <cpu.h>
 #include <gates.h>
 #include "tsyscall.h"
 #include "tsystm.h"
 #include "proc.h"
 #include "sched.h"
+#include <string.h>
+
+#define MILLION 1000000
+#define DELAY (400 * MILLION)
 
 extern void ustart1(void);
 extern void ustart2(void);
 extern void ustart3(void);
 
+void init_proctab(void);
+void process0(void);
+void delay(void);
+
+PEntry proctab[NPROC], *curproc;
+
 extern IntHandler syscall; /* the assembler envelope routine    */
 extern void finale(void);
 /* kprintf is proto'd in stdio.h, but we don't need that for anything else */
-//void kprintf(char *, ...);	
+void kprintf(char *, ...);	
 
 /* functions in this file */
 void udebug_set_trap_gate(int n, IntHandler *inthand_addr, int debug);
@@ -24,9 +33,6 @@ int sysexit(int);
 void k_init(void);
 void shutdown(void);
 void syscallc(int user_eax, int devcode, char *buff , int bufflen);
-
-PEntry proctab[NPROC], *curproc;
-void init_proctab(void);
 
 /* Record debug info in otherwise free memory between program and stack */
 /* 0x300000 = 3M, the start of the last M of user memory on the SAPC */
@@ -62,28 +68,31 @@ void k_init(){
   sysent[TREAD].sy_narg = 3;
   sysent[TIOCTL].sy_narg = 3;
   sysent[TWRITE].sy_narg = 3;
-  sti();			/* user runs with interrupts on */
-  init_proctab();
-  schedule();
+	sti(); /* user runs with interrupts on */
+	init_proctab();
+	process0();
 }
 
-void init_proctab(void) {
-  proctab[1].p_savedregs[SAVED_ESP] = ESP1;
-  proctab[2].p_savedregs[SAVED_ESP] = ESP2;
-  proctab[3].p_savedregs[SAVED_ESP] = ESP3;
+void init_proctab() {
+	int i;
 
-  proctab[1].p_savedregs[SAVED_PC] = (int) &ustart1;
-  proctab[2].p_savedregs[SAVED_PC] = (int) &ustart2;
-  proctab[3].p_savedregs[SAVED_PC] = (int) &ustart3;
+	proctab[1].p_savedregs[SAVED_PC] = (int) &ustart1;
+	proctab[2].p_savedregs[SAVED_PC] = (int) &ustart2;
+	proctab[3].p_savedregs[SAVED_PC] = (int) &ustart3;
 
-  for (int i = 0; i < NPROC; i++) {
-    proctab[i].p_savedregs[SAVED_EFLAGS] = 0x1 << 9;
-    proctab[i].p_savedregs[SAVED_EBP] = 0;  			
-	  proctab[i].p_status = RUN;
-  }
+	proctab[1].p_savedregs[SAVED_ESP] = ESP1;
+	proctab[2].p_savedregs[SAVED_ESP] = ESP2;
+	proctab[3].p_savedregs[SAVED_ESP] = ESP3;
 
-  curproc = &proctab[1];
+	for (i = 0; i < NPROC; i++) {
+		proctab[i].p_savedregs[SAVED_EFLAGS] = 0x1 << 9;
+    proctab[i].p_savedregs[SAVED_EBP] = 0;
+		proctab[i].p_status = RUN;
+	}
+
+	curproc = &proctab[0];
 }
+
 
 /* shut the system down */
 void shutdown()
@@ -130,6 +139,7 @@ void syscallc( int user_eax, int devcode, char *buff , int bufflen)
 /****************************************************************************/
 
 int sysexit(int exit_code){
+  cli();
   curproc->p_exitval = exit_code;
   curproc->p_status = ZOMBIE;
   schedule();
@@ -181,4 +191,20 @@ void debug_log(char *msg)
     debug_record +=strlen(msg);
 }
 
+void process0() {
+	while ((proctab[1].p_status == RUN) || (proctab[2].p_status == RUN) ||
+        (proctab[3].p_status == RUN)) {
+	  schedule();
+	}
+  delay();      
+	shutdown();
+}
 
+
+void delay() {
+  int i;
+
+  for (i = 0; i < DELAY; i++) {
+  
+  }
+}
